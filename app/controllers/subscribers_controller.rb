6eec82b7a213c794da2829e5a83575c415ae385d
David Green <David.e.green@gmail.com>
2016-08-10 21:11:17 -0400

    Mailer, layout

--- app/controllers/subscribers_controller.rb
+++ app/controllers/subscribers_controller.rb
@@ -7,16 +7,17 @@ class SubscribersController < ApplicationController
 	def create
 		@subscriber = Subscriber.new(subscriber_params)
 		if @subscriber.save
-			FsboMailer.send_signup_email(@user).deliver
+			flash[:success] = "Thank you for subscribing! Your E-Guide is On It's Way."
+			FsboMailer.send_fsbo_report_email(@subscriber).deliver_now
 			redirect_to subscribers_path
-		else
-			render 'new'
+		else	
+			render 'index'
 		end
 
 	end
 
 	def subscriber_params
-		params.require(:subscriber).permit(:email)
+		params.require(:subscriber).permit(:email, :first_name, :last_name)
 	end
 
 end
--- app/mailers/fsbo_mailer.rb
+++ app/mailers/fsbo_mailer.rb
@@ -1,12 +1,12 @@
 class FsboMailer < ApplicationMailer
 
-	default :from => 'fsbo@gmail.com'
+	default from: 'For Sale By Owner Guide <fsbo@gmail.com>'
 
   # send a signup email to the user, pass in the user object that   contains the user's email address
   def send_fsbo_report_email(user)
     @subscriber = user
-    mail( :to => @subscriber.email,
-    :subject => "Here's your Free FSBO Cheatsheet" )
+    mail(to: @subscriber.email, subject: "Here's your Free FSBO Guide")
+    attachments['For_Sale_By_Owner_Guide.pdf'] = File.read('app/assets/images/2015_state_of_email.pdf')
   end
 
 end
