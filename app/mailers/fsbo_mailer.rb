91790d5c2560445417d4f129ddfde7e2c2783694
David Green <David.e.green@gmail.com>
2016-08-12 16:16:38 -0400

    Add booklet

--- app/mailers/fsbo_mailer.rb
+++ app/mailers/fsbo_mailer.rb
@@ -5,7 +5,7 @@ class FsboMailer < ApplicationMailer
   # send a signup email to the user, pass in the user object that   contains the user's email address
   def send_fsbo_report_email(user)
     @subscriber = user
-    attachments['For_Sale_By_Owner_Guide.pdf'] = File.read('app/assets/images/2015_state_of_email.pdf')
+    attachments['FSBO_Guide.pdf'] = File.read('app/assets/images/2015_state_of_email.pdf')
     mail(to: @subscriber.email, subject: "Here's your Free FSBO Guide")
   end
 
